3e872d6dd4dca86efa24cd62c4048dcf8605186f
Felipe Reis <felipeareis@protonmail.com>
2021-11-03 23:44:36 +0000

    finished challenge

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,12 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: calc(list, 0)
+
+  def calc([], acc) do
+    acc
+  end
+
+  def calc([_head | tail], acc) do
+    acc = acc + 1
+    calc(tail, acc)
   end
 end
